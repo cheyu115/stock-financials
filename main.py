f1a98fa0c5b6e0717133de2a98fe8975a3087eb1
che-yu kuo <factorybot87@gmail.com>
2026-02-14 12:53:45 +0800

    feat: get user input and print result

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from stock-financials!")
+from stock import *
 
+def format_output(price: float, eps: float, eps_growth: float) -> None:
+    pe_ratio = calculate_pe_ratio(price, eps)
+    print("PE ratio:\t", pe_ratio)
+    print("PEG ratio: \t", calculate_peg_ratio(pe_ratio, eps_growth))
+
+def main():
+    price = float(input("price: "))
+    eps = float(input("eps: "))
+    eps_growth = float(input("growth rate (%): "))
+
+    format_output(price, eps, eps_growth)
 
 if __name__ == "__main__":
     main()
