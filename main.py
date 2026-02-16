8783456830bfb931500627ebda59e9d73afbaca7
che-yu kuo <factorybot87@gmail.com>
2026-02-16 11:37:43 +0800

    feat: use StockStatistics DTO

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-from stock import *
-
-def format_output(price: float, eps: float, eps_growth: float) -> None:
-    pe_ratio = calculate_pe_ratio(price, eps)
-    print("PE ratio:\t", pe_ratio)
-    print("PEG ratio: \t", calculate_peg_ratio(pe_ratio, eps_growth))
+import datetime
+from stock import StockStatistics, calculate_pe_ratio, calculate_peg_ratio
 
 def main():
+    ticker  = input("stock: ").upper()
     price = float(input("price: "))
     eps = float(input("eps: "))
     eps_growth = float(input("growth rate (%): "))
+    pe_ratio = calculate_pe_ratio(price, eps)
+    peg_ratio = calculate_peg_ratio(pe_ratio, eps_growth)
+    today_str = str(datetime.date.today())
 
-    format_output(price, eps, eps_growth)
+    record = StockStatistics(ticker=ticker, price=price, eps=eps, eps_growth=eps_growth, pe_ratio=pe_ratio, peg_ratio=peg_ratio, date=today_str)
 
+    print(record)
 if __name__ == "__main__":
+    print("test data: aapl, 156, 7.9, 9.46")
     main()
